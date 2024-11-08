84dab6f994d9fd9964795610db06a690b868d4dd
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2024-11-08 12:38:02 +0100

    feat: add movement to agents

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,7 @@ multiJvm {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(libs.bundles.alchemist.protelis)
+    implementation(libs.bundles.alchemist.euclidean.geometry)
     if (!GraphicsEnvironment.isHeadless()) {
         implementation("it.unibo.alchemist:alchemist-swingui:${libs.versions.alchemist.get()}")
     }
