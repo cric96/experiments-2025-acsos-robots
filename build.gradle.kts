73fb6a205693bf43c5ded8097ebcce4a109aff72
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2025-04-18 17:24:49 +0200

    fix(build): remove java launcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,11 +98,6 @@ File(rootProject.rootDir.path + "/src/main/yaml").listFiles()
             mainClass.set("it.unibo.alchemist.Alchemist")
             classpath = sourceSets["main"].runtimeClasspath
             args("run", it.absolutePath)
-            javaLauncher.set(
-                javaToolchains.launcherFor {
-                    languageVersion.set(JavaLanguageVersion.of(multiJvm.latestJava))
-                },
-            )
             if (System.getenv("CI") == "true") {
                 args("--override", "terminate: { type: AfterTime, parameters: [2] } ")
             } else {
