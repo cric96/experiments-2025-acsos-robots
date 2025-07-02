d67aadd546fc7552b069ad1d23e89a0077a6294b
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-07-02 17:19:46 +0200

    fix: add env variable to gradle command

--- runtime-replanning-leader-based.sh
+++ runtime-replanning-leader-based.sh
@@ -2,4 +2,4 @@
 DESTINATION="$HOME/Downloads/runtime-replanning-leader-based-$(date --utc "+%F-%H.%M.%S")"
 git clone https://github.com/angelacorte/experiments-2025-acsos-robots "$DESTINATION"
 cd "$DESTINATION"
-MAX_SEED=0 ./gradlew runDepotsRuntimeGraphic
+MAX_SEED=0 LEADER_BASED=true ./gradlew runDepotsRuntimeGraphic
