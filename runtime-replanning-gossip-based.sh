0b1bd7d0d2262cb176fe75af44485278e4757b9a
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-07-02 17:20:09 +0200

    feat: add fast launcher script for gossip based

---
+++ runtime-replanning-gossip-based.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+DESTINATION="$HOME/Downloads/runtime-replanning-leader-based-$(date --utc "+%F-%H.%M.%S")"
+git clone https://github.com/angelacorte/experiments-2025-acsos-robots "$DESTINATION"
+cd "$DESTINATION"
+MAX_SEED=0 LEADER_BASED=false ./gradlew runDepotsRuntimeGraphic
